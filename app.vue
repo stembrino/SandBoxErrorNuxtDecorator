0456bd636e45247421ef9bb4303411b70d4394af
fabiofcs <fcarvalho@primeit.pt>
2023-11-16 16:50:49 +0000

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Test />
+  </div>
+</template>
+<script >
+import Test from "./components/Test.vue"
+</script>
---
+++ components/Test.vue
@@ -0,0 +1,16 @@
+
+<template>
+  {{ helloWorld }}
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-facing-decorator";
+
+@Component
+export default class Test extends Vue {
+ helloWorld = "hello world";
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
